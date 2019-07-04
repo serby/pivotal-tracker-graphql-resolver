35d59b1aebeded14359fbfdad199d7083839ba9f
Paul Serby <paul.serby@clock.co.uk>
2019-07-04 18:24:01 +0100

    No points by default

--- markdowner.js
+++ markdowner.js
@@ -25,29 +25,28 @@ const query = `
         url
         title
         points
-        comments {
-          text
-          author
-        }
       }
     }
   }
 }
 `
 
-const encodeHtml = str => str.replace(/[\u00A0-\u9999<>&](?!#)/gim, i => '&#' + i.charCodeAt(0) + ';')
+const encodeHtml = str =>
+  str.replace(/[\u00A0-\u9999<>&](?!#)/gim, i => '&#' + i.charCodeAt(0) + ';')
 
 const nl2br = input => input.replace(/\n/g, '\n\n')
 
 const markedNl2br = input => marked(nl2br(encodeHtml(input)))
 
 const typeToEmoji = type => {
-  return {
-    chore: '⚙️',
-    feature: '⭐️',
-    story: '⭐️',
-    bug: '🐞'
-  }[type] || `[${type}]`
+  return (
+    {
+      chore: '⚙️',
+      feature: '⭐️',
+      story: '⭐️',
+      bug: '🐞'
+    }[type] || `[${type}]`
+  )
 }
 
 const unitMap = {
@@ -70,16 +69,19 @@ const token = process.env.TOKEN
 graphql(createSchema(token), query).then(response => {
   if (response.errors) return console.error(response.errors)
   const { project } = response.data
-  const stats = project.epics.reduce((stats, epic) => {
-    stats.count += epic.stories.length
-    stats.epics += 1
-    epic.sum = epic.stories.reduce((sum, story) => {
-      sum += story.points || 0
-      return sum
-    }, 0)
-    stats.sum += epic.sum
-    return stats
-  }, { count: 0, sum: 0, epics: 0 })
+  const stats = project.epics.reduce(
+    (stats, epic) => {
+      stats.count += epic.stories.length
+      stats.epics += 1
+      epic.sum = epic.stories.reduce((sum, story) => {
+        sum += story.points || 0
+        return sum
+      }, 0)
+      stats.sum += epic.sum
+      return stats
+    },
+    { count: 0, sum: 0, epics: 0 }
+  )
 
   const memberIndex = project.membership.reduce((index, member) => {
     index[member.id] = member.username
@@ -92,7 +94,11 @@ graphql(createSchema(token), query).then(response => {
 <h1>${project.name}</h1>
 <p><a href="${project.url}">${project.url}</a></p>
 ${project.description ? '<p>project.description</p>' : ''}
-<p>This project has <strong>${stats.sum}</strong> ${displayUnit(stats.sum).toLowerCase()} in <strong>${stats.count}</strong> stories contained in <strong>${stats.epics}</strong> epics</p>
+<p>This project has <strong>${stats.sum}</strong> ${displayUnit(
+    stats.sum
+  ).toLowerCase()} in <strong>${
+    stats.count
+  }</strong> stories contained in <strong>${stats.epics}</strong> epics</p>
 ${project.epics.map(convertEpicToHtml(memberIndex)).join('\n')}
 </body>
 </html>
@@ -107,11 +113,16 @@ const convertEpicToHtml = memberIndex => epic => {
 <p><a href="${epic.url}">${epic.url}</a></p>
 ${displayPoints ? `<p>Epic Points: ${epic.sum}</p>` : ''}
 ${epic.description ? markedNl2br(epic.description) : ''}
-${epic.stories.length > 0 ? `<ol>${epic.stories.map(convertStoryToHtml(memberIndex)).join('')}</ol>` : ''}
+${
+  epic.stories.length > 0
+    ? `<ol>${epic.stories.map(convertStoryToHtml(memberIndex)).join('')}</ol>`
+    : ''
+}
 `
 }
 
-const convertCommentToHtml = memberIndex => comment => comment.text ? `<p>@${memberIndex[comment.author]} - ${comment.text}</p>` : ''
+const convertCommentToHtml = memberIndex => comment =>
+  comment.text ? `<p>@${memberIndex[comment.author]} - ${comment.text}</p>` : ''
 
 const convertCommentsToHtml = (memberIndex, comments) => {
   if (comments && comments.length > 0) {
@@ -122,6 +133,14 @@ const convertCommentsToHtml = (memberIndex, comments) => {
 }
 
 const convertStoryToHtml = memberIndex => story => `
-<li>${markedNl2br(typeToEmoji(story.type) + ' ' + `${story.title} [${story.id}](${story.url}) ${displayPoints && story.points ? `- ${story.points} ${displayUnit(story.points)}` : ''}`)}
+<li>${markedNl2br(
+  typeToEmoji(story.type) +
+    ' ' +
+    `${story.title} [${story.id}](${story.url}) ${
+      displayPoints && story.points
+        ? `- ${story.points} ${displayUnit(story.points)}`
+        : ''
+    }`
+)}
 ${story.description ? `${markedNl2br(story.description)}` : ''}
-${convertCommentsToHtml(memberIndex, story.comments)}`
+${story.comments ? convertCommentsToHtml(memberIndex, story.comments) : ''}`
